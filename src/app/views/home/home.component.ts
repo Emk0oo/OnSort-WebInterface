e6bd1ad4b39578d2bbc36f2ed11d279a5196f312
Javor Emir <emir.j2002@gmail.com>
2024-12-18 15:02:59 +0100

    feat: add download component to home page and update presentation layout

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -4,9 +4,11 @@ import { PresentationComponent } from '../../components/home/presentation/presen
 import { DetailsComponent } from '../../components/home/details/details.component';
 import { PartenairesComponent } from '../../components/home/partenaires/partenaires.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { DownloadComponent } from '../../components/home/download/download.component';
+
 @Component({
   selector: 'app-home',
-  imports: [PresentationComponent, DetailsComponent, PartenairesComponent, FooterComponent, CommonModule],
+  imports: [PresentationComponent, DetailsComponent, PartenairesComponent, DownloadComponent, FooterComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
