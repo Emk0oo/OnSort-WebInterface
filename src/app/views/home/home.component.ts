e7f2a39c2d380571907c7daa61a439a96d3756a5
Javor Emir <emir.j2002@gmail.com>
2024-12-18 14:26:00 +0100

    feat: add footer and partenaires components to enhance home page layout and structure

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 import { PresentationComponent } from '../../components/home/presentation/presentation.component';
 import { DetailsComponent } from '../../components/home/details/details.component';
+import { PartenairesComponent } from '../../components/home/partenaires/partenaires.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 @Component({
   selector: 'app-home',
-  imports: [PresentationComponent, DetailsComponent],
+  imports: [PresentationComponent, DetailsComponent, PartenairesComponent, FooterComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
