221591876d22acbfdbe0c9da8e0b1b7e699d9ad9
Javor Emir <emir.j2002@gmail.com>
2024-12-19 11:25:35 +0100

    feat: add newsletter component to home page for user subscriptions

---
+++ src/app/components/home/newsletter/newsletter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-newsletter',
+  imports: [CommonModule],
+  templateUrl: './newsletter.component.html',
+  styleUrl: './newsletter.component.scss'
+})
+export class NewsletterComponent {
+
+}
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -4,12 +4,13 @@ import { PresentationComponent } from '../../components/home/presentation/presen
 import { DetailsComponent } from '../../components/home/details/details.component';
 import { PartenairesComponent } from '../../components/home/partenaires/partenaires.component';
 import { DownloadComponent } from '../../components/home/download/download.component';
+import { NewsletterComponent} from '../../components/home/newsletter/newsletter.component';
 import { FaqComponent } from '../../components/home/faq/faq.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [PresentationComponent, DetailsComponent, PartenairesComponent, DownloadComponent, FaqComponent, FooterComponent, CommonModule],
+  imports: [PresentationComponent, DetailsComponent, PartenairesComponent, DownloadComponent, FaqComponent, NewsletterComponent, FooterComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
