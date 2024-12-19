aa6042a09e0f9034552c9a5ede4858ee1e411672
Javor Emir <emir.j2002@gmail.com>
2024-12-19 13:57:11 +0100

    image details

--- src/app/components/home/details/details.component.ts
+++ src/app/components/home/details/details.component.ts
@@ -11,17 +11,17 @@ import { CommonModule } from '@angular/common';
 export class DetailsComponent {
   details = [
     {
-      image: 'https://placehold.co/100x100',
+      image: 'loupe.png',
       title: 'Un accélérateur de découvertes',
       description: 'Faites connaissance avec des lieux et des activités que vous n’auriez pas trouvés autrement.',
     },
     {
-      image: 'https://placehold.co/100x100',
+      image: 'shop.png',
       title: 'Un boost pour l’économie locale',
       description: 'Nous mettons en avant les restaurants, cinémas et autres activités près de chez vous pour soutenir les commerçants et entreprises locales.',
     },
     {
-      image: 'https://placehold.co/100x100',
+      image: 'humans.png',
       title: 'Une expérience connectée et humaine',
       description: 'Sortir, c’est avant tout partager des moments avec ceux qui comptent pour vous.',
     },
