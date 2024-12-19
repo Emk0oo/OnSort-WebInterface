c3278e35e80dfbc2c90540618da4f10346a04c38
Javor Emir <emir.j2002@gmail.com>
2024-12-19 13:19:20 +0100

    angular json config and assets

--- src/app/components/home/faq/faq.component.ts
+++ src/app/components/home/faq/faq.component.ts
@@ -10,19 +10,31 @@ import { CommonModule } from '@angular/common';
 export class FaqComponent {
   faqs = [
     {
-      title: 'D’où proviennent nos produits reconditionnés ?',
+      title: "Qu'est-ce que OnSort ?",
       content:
-        'Tous nos produits sont issus du marché français. Ils proviennent de particuliers, de l’opérateur Orange, ou encore d’entreprises françaises. Ceci nous permet d’assurer à nos clients des produits en totale conformité avec les normes de notre pays, mais aussi de limiter significativement l’empreinte environnementale liée au transport des produits. Tous les produits que nous récupérons sont ensuite entièrement contrôlés et reconditionnés dans nos ateliers de Caen et de Toulouse. Une économie circulaire 100% française qui participe à la création d’emplois locaux et à la valorisation du Made in France !',
+        "OnSort est une application collaborative qui simplifie l'organisation des sorties entre amis. Elle permet de choisir facilement une activité, planifier une date, gérer le budget et partager les paiements grâce à des outils intégrés de sondage et de cagnotte.",
       isOpen: true,
     },
     {
-      title: 'Que couvre notre garantie de 2 ans ?',
-      content: 'Contenu de la deuxième question.',
+      title: 'Comment fonctionne OnSort ?',
+      content: `
+        <p>Recherche d'activités : Explorez une sélection d'activités locales (restaurants, cinémas, concerts, etc.).</p>
+        <p>Création d'un sondage : Proposez des options pour la date, l'activité ou le budget.</p>
+        <p>Invitations : Partagez un lien avec vos amis pour qu'ils votent ou valident leur participation.</p>
+      `,
       isOpen: false,
     },
     {
-      title: 'Comment nous contacter en cas de besoin ?',
-      content: 'Contenu de la troisième question.',
+      title: "Quels types d'activités puis-je organiser avec OnSort ?",
+      content: `
+        <p>Vous pouvez organiser une large gamme d'activités, comme :</p>
+        <ul class="list-disc pl-6">
+          <li>Des sorties culturelles (musées, expositions).</li>
+          <li>Des soirées au restaurant ou au bar.</li>
+          <li>Des loisirs en groupe (escape games, bowling).</li>
+          <li>Des événements ponctuels (anniversaires, célébrations).</li>
+        </ul>
+      `,
       isOpen: false,
     },
   ];
